8566ba37687d3fc14654173ae1c66eaf423f96ac
germanps-home <germanps@outlok.es>
2017-08-24 20:21:35 +0200

    Variables & tipos de datos

--- src/js/01_class01.js
+++ src/js/01_class01.js
@@ -1,11 +1,46 @@
-//Javascript es CaseSensitive
+
+/*Javascript es CaseSensitive*/
 var a = "Alumno",
     b = "Alumno";
 
 console.log(a === b);
 
-//Tipos de datos
+/*Tipos de datos*/
 var c = 5;
 console.log(typeof(c));
 c = 'Hola';
 console.log(typeof(c));
+var despedirse = function(){
+	console.log("Adios");
+	console.log("Esto es de tipo: " + typeof(this));
+}; 
+despedirse();
+
+/*Variables*/
+var nombre = "Germán";
+console.log(nombre);
+var noDefinida;
+console.log(noDefinida);
+noDefinida = "Ahora tengo valor!";
+console.log(noDefinida);
+function saludar(){
+	var saludo = "Hola!";
+	console.log(saludo);
+}
+
+/*Tipos de datos*/
+//Primitivos => Un solo valor(Primarios y especiales)
+/*
+	*Primarios{
+		Number => Enteros, decimales, negataivos, etc
+	}
+	*String
+	*Boolean => true or false {
+		Falsy values
+		-NaN, 0, "",Null(valor inexistente),Undefined(no tiene valor o no lo retorna)
+	}
+
+*/
+
+//Compuestos => mas de un valor(arrays y objetos)
+
