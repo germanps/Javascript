2587d88af4dc068fe573ab252feb75ad45336c34
German <germanps@outlook.es>
2017-09-07 16:38:41 +0200

    Metodos Arrays

--- src/js/03_arrays.js
+++ src/js/03_arrays.js
@@ -19,9 +19,47 @@ var arr2 = arr1.join('-');
 console.log(arr2);
 
 //Con convertimos un String en un Array
-var miString = 'fantastico';
+var miString = 'Fué un dia fantastico';
 var string2Array = miString.split('');
 console.log(string2Array);
+console.log(string2Array[4]);
+
+//Concatenación de arrays
+var arr3 = ['a', 'b', 'c', 'd'];
+var arr4 = ['e', 'f', 'g', 'h'];
+var arraConcat = arr3.concat(arr4);
+console.log(arraConcat);
+
+//Añadir elementos al INICIO
+arr3.unshift('Al', 'Principio');
+console.log(arr3);
+
+//Añadir elementos al FINAL
+arr4.push('Al', 'Final');
+console.log(arr4);
+
+//Quitar elementos de PRINCIPIO (te lo retorna)
+var newArray = [1, 2, 3, 4, 5];
+
+var elemento1 = newArray.shift()
+console.log(elemento1);
+console.log(newArray);
+
+//Quitar elementos del Final (te lo retorna)
+var elemento2 = newArray.pop();
+console.log(elemento2);
+console.log(newArray);
+
+//Crear otro array partiendo de otro array
+var newArray2 = [1, 2, 3, 4, 5];
+var newArray3 = newArray2.slice(1, 3);
+console.log(newArray3);
+
+//Modificar Arrays
+var newArray3 = [1, 2, 3, 4, 5];
+var newArray4 = newArray3.splice(1, 2);
+console.log(newArray4);
+console.log(newArray3);
 
 
 //Recorrer todo el array e imprimirlo
